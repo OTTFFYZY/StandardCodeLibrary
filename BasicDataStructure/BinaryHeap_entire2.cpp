955434c156f8241ff06711db88899ce0af40e7ac
OTTFFYZY <992241461@qq.com>
2018-04-20 22:55:46 +0800

    2018_04_20_upd_2

--- BasicDataStructure/BinaryHeap_entire2.cpp
+++ BasicDataStructure/BinaryHeap_entire2.cpp
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #include <algorithm>
 using namespace std;
 
@@ -8,63 +9,59 @@ struct Heap
 {
 	int h[M],n,cnt;// n is the number of element in the heap
 	               // cnt is the number of element push in the heap 
-	int id[M],pos[M]; //
+	int pos[M];    // the position of val in the heap
 	Heap() :n(0),cnt(0) {}
 	Heap(int a[],int nn) :n(0),cnt(nn)
 	{
+		memset(pos,0,sizeof(pos));
 		for(int i=0;i<nn;i++)
 		{
 			h[++n]=a[i];
-			id[n]=pos[n]=n;
+			pos[h[n]]=n;
 		}
 		for(int i=n/2;i>=1;i--) sink(i);
 	}
 	void swim(int p)
 	{
-		int q=p>>1,x=h[p],y=id[p];
+		int q=p>>1,x=h[p];
 		while(q&&x<h[q])
 		{
 			h[p]=h[q];
-			id[p]=id[q];
-			pos[id[p]]=p;
+			pos[h[p]]=p;
 			p=q;
 			q=p>>1;
 		}
 		h[p]=x;
-		id[p]=y;
-		pos[y]=p;
+		pos[x]=p;
 	}
 	void sink(int p)
 	{
-		int q=p<<1,x=h[p],y=id[p];
+		int q=p<<1,x=h[p];
 		while(q<=n)
 		{
 			if(q<n&&h[q+1]<h[q]) q++;
 			if(h[q]>=x) break;
 			h[p]=h[q];
-			id[p]=id[q];
-			pos[id[p]]=p;
+			pos[h[p]]=p;
 			p=q;
 			q=p<<1;
 		}
 		h[p]=x;
-		id[p]=y;
-		pos[y]=p;
+		pos[x]=p;
 	}
 	
 	void push(int x)
 	{
 		h[++n]=x;
-		id[n]=++cnt;
-		pos[id[n]]=n;
+		pos[x]=n;
 		swim(n);
 	}
 	int pop()
 	{
 		int r=h[1];
-		swap(h[1],h[n]);
-		swap(id[1],id[n--]);
-		pos[id[1]]=1;
+		swap(h[1],h[n--]);
+		pos[h[1]]=1;
+		pos[r]=0;
 		sink(1);
 		return r;
 	}
@@ -72,17 +69,20 @@ struct Heap
 	{
 		h[pos[i]]=INT_MIN;
 		swim(pos[i]);
+		pos[i]=0;
 		pop();
 	}
-	void change(int i,int val)
+	void change(int x,int y)//change v from x to y;
 	{
-		h[pos[i]]=val;
-		sink(pos[i]);
-		swim(pos[i]);
+		h[pos[x]]=y;
+		pos[y]=pos[x];
+		pos[x]=0;
+		sink(pos[y]);
+		swim(pos[y]);
 	}
-	int get_kpush(int k)
+	int isk_inh(int k)
 	{
-		return h[pos[k]];
+		return pos[k];
 	}
 
 };
