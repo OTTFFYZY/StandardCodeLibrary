061c1f0c362680dc1f91a8e087ebd9b1795f7d4a
OTTFFYZY <992241461@qq.com>
2018-06-22 23:58:08 +0800

    2018_06_22_upd_0

---
+++ Mathematics/NumberTheory/PrimeTable_0.cpp
@@ -0,0 +1,27 @@
+const int M=1e6+5;
+
+int isp[M],pri[M],npri=0;
+
+//nlogn
+void getpri0(int n)
+{
+	//isp[0]=isp[1]=1;
+	for(int i=2;i<=n;i++)
+		if(!isp[i])
+		{
+			pri[npri++]=i;
+			for(int j=i+i;j<=n;j+=i)
+				isp[j]=1;
+		}
+}
+
+void getpri1(int n)
+{
+	//isp[0]=isp[1]=1;
+	for(int i=2;i*i<=n;i++)    //i*i <=n <= INT_MAX
+		if(!isp[i])
+			for(int j=i*i;j<=n;j+=i) 
+				isp[j]=1;
+	for(int i=2;i<=n;i++)
+		if(!isp[i]) pri[npri++]=i;
+}
