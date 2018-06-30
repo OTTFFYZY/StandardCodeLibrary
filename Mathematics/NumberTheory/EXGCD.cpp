437f658ee35e6254114c51b2c8246096fe51c9ba
OTTFFYZY <992241461@qq.com>
2018-06-30 21:38:28 +0800

    2018_06_30_upd_0

--- Mathematics/NumberTheory/EXGCD.cpp
+++ Mathematics/NumberTheory/EXGCD.cpp
@@ -4,8 +4,7 @@ void exgcd(LL a,LL b,LL &d,LL &x,LL &y) // ax+by=gcd(a,b)=d
 {
 	if(!b)
 	{
-		d=a;
-		x=1; y=0;
+		d=a; x=1; y=0;
 	}
 	else
 	{
@@ -20,7 +19,14 @@ void exgcd(LL a,LL b,LL &d,LL &x,LL &y) // ax+by=gcd(a,b)=d
 // n=a/g  m=b/g
 // x'=x'0+km  y'=y'0-kn
 // x=c/g *x'  y=c/g *y'
-
-
+bool solve(LL a,LL b,LL c,LL &x,LL &y)
+{
+    LL g;
+    exgcd(a,b,g,x,y);
+    if(c%g) return 0;
+    x*=c/g;
+    y*=c/g;
+    return 1;
+}
 
 // ax=b(mod m) => ax+my=b
