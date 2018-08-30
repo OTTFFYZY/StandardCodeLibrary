62af80960dbc7babef8936dfa12ea8c1aaa57ddd
OTTFFYZY <992241461@qq.com>
2018-08-29 23:33:07 -0400

    2018_08_29_upd_0

--- IntermediateDataStructure/WeightSegmenTree_pointer.cpp
+++ IntermediateDataStructure/WeightSegmenTree_pointer.cpp
@@ -4,7 +4,7 @@
 
 using namespace std;
 
-const int M=1e5+5;
+const int M=1e3+5;
 struct TN
 {	
 	TN *ls,*rs;
@@ -20,23 +20,23 @@ namespace MP
 	}
 	TN* add(int l,int r)
 	{
-		TN *o=pb?b[--pb]:&a[pa];
+		TN *o=pb?b[--pb]:&a[pa++];
 		o->l=l; o->r=r;
 		o->ls=o->rs=NULL;
 		o->si=0;
 		return o;
 	}
-	void del(T *x)
+	void del(TN *x)
 	{
 		b[pb++]=x;
 	}
 }
 
+TN *root;
 struct SegT
 {
-	TN *root;
 	const static int WMIN=1;
-	const static int WMAX=M;
+	const static int WMAX=20;
 	void init()
 	{
 		MP::clear();
@@ -72,10 +72,10 @@ struct SegT
 	{
 		if(l==r) return l;
 		int m=(l+r)/2;
-		if(o->si>=k) 
+		if(o->ls&&o->ls->si>=k)
 			return kth(k,o->ls,l,m);
-		else 
-			return kth(k-o->ls->si,o->rs,m+1,r);
+		else
+			return kth(k-(o->ls?o->ls->si:0),o->rs,m+1,r);
 	}
 	int rank(int x,TN *o=root,int l=WMIN,int r=WMAX) // get the rank of x
 	{
@@ -84,7 +84,7 @@ struct SegT
 		if(x<=m) 
 			return rank(x,o->ls,l,m);
 		else 
-			return o->ls.si+rank(x,o->rs,m+1,r);
+			return (o->ls?o->ls->si:0)+rank(x,o->rs,m+1,r);
 	}
 	int count(int x,TN *o=root,int l=WMIN,int r=WMAX) // get the number of x
 	{
@@ -114,8 +114,9 @@ int main()
 	segt.ins(5,3);
 	segt.ins(15,1);
 	segt.ins(12,1);
+	cout<<"ok "<<(root->si)<<endl;
 	cout<<"kth(20) "<<segt.kth(20)<<" kth(21) "<<segt.kth(21)<<endl;
-	cout<<"kth(0) "<<segt.kth(0)<<" kth(25) "<<segt.kth(25)<<" kth(26) "<<segt.kth(26)<<endl;
+	cout<<"kth(1) "<<segt.kth(1)<<" kth(25) "<<segt.kth(25)<<endl;//" kth(26) "<<segt.kth(26)<<endl;
 	cout<<"rank(1) "<<segt.rank(1)<<" rank(2) "<<segt.rank(2)<<endl;
 	cout<<"rank(0) "<<segt.rank(0)<<" rank(5) "<<segt.rank(5)<<" rank(15) "<<segt.rank(15)<<endl;
 	return 0;
