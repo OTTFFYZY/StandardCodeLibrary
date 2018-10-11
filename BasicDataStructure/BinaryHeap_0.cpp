a37fabc45e425f9c1934612b3e3e8ad3a9a75e10
OTTFFYZY <992241461@qq.com>
2018-10-10 23:36:27 -0400

    2018_10_10_upd_0

--- BasicDataStructure/BinaryHeap_0.cpp
+++ BasicDataStructure/BinaryHeap_0.cpp
@@ -5,8 +5,12 @@ const int M=1e5+5;
 
 struct Heap
 {
-	int h[M],n;
-	Heap() :n(0) {}
+	int h[M],n; // root at h[1]
+	Heap(int n=0):n(n) // O(n) 
+	{
+		for(int i=n/2;i>0;i--)
+			sink(i);
+	}
 	void swim(int p)
 	{
 		int q=p>>1,x=h[p];
