183fe0bb609a8ddbf4deff6cad918f32acd0ca0d
OTTFFYZY <992241461@qq.com>
2019-01-01 21:55:56 +0800

    2019_01_01_upd_0

--- GraphTheory/Theorem.md
+++ GraphTheory/Theorem.md
@@ -38,3 +38,40 @@ $$
 L: number of leaf nodes
 
 T: number of nodes with two children
+
+
+
+
+
+## Graph & Digraph & Bipartite Realization Problem
+
+### Erdős-Gallai Theorem
+
+It provides one of two known approaches to solving the graph realization problem, i.e. it gives a necessary and sufficient condition for a finite sequence of natural numbers to be the degree sequence of a simple graph. A sequence obeying these conditions is called "graphic".
+
+A sequence of non-negative integers $ d_{1} \geq \cdots \geq d_{n} $ can be represented as the degree sequence of a finite simple graph on n vertices if and only if $d_1+\cdots+d_n$ is even and
+$$
+\sum_{i=1}^{k} d_i \leq k(k+1)+\sum_{i=k+1}^{n} min(d_i,k)
+$$
+for every k in $1 \leq k \leq n$.
+
+
+
+### Fulkerson–Chen–Anstee Theorem
+
+It provides one of two known approaches solving the digraph realization problem, i.e. it gives a necessary and sufficient condition for pairs of nonnegative integers $ ((a_{1},b_{1}),\ldots ,(a_{n},b_{n}))$ to be the indegree-outdegree pairs of a simple directed graph; a sequence obeying these conditions is called "digraphic".
+
+A sequence $((a_{1},b_{1}),\ldots ,(a_{n},b_{n}))$ of nonnegative integer pairs with $a_{1}\geq \cdots \geq a_{n}$ is digraphic if and only if $\sum _{i=1}^{n}a_{i}=\sum _{i=1}^{n}b_{i}$ and the following inequality holds for k such that $1\leq k\leq n$:
+$$
+\sum_{i=1}^{k} a_{i} \leq \sum_{i=1}^{k} min(b_{i},k-1) + \sum_{i=k+1}^{n} min(b_{i},k)
+$$
+
+
+### Gale–Ryser theorem
+
+It provides one of two known approaches to solving the bipartite realization problem, i.e. it gives a necessary and sufficient condition for two finite sequences of natural numbers to be the degree sequence of a labeled simple bipartite graph; a sequence obeying these conditions is called "bigraphic". It is an analog of the Erdős–Gallai theorem for simple graphs.
+
+A sequence $((a_{1},b_{1}),\ldots ,(a_{n},b_{n}))$ of nonnegative integer pairs with $a_{1}\geq \cdots \geq a_{n}$ is bigraphic if and only if $\sum _{i=1}^{n}a_{i}=\sum _{i=1}^{n}b_{i}$ and the following inequality holds for k such that $1\leq k\leq n$:
+$$
+\sum_{i=1}^{k} a_{i} \leq \sum_{i=1}^{n} min(b_{i},k)
+$$
--- Mathematics/SpecialNumber/BigInt_0.cpp
+++ Mathematics/SpecialNumber/BigInt_0.cpp
@@ -1,26 +1,34 @@
 #include <iostream>
 #include <stdio.h>
 #include <string.h>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <time.h>
+#include <stdlib.h>
+using namespace std;
 
 typedef long long LL;
 
 const int M=10005;
 const int MB=4;
 const int BASE=10000;
-struct BN
+char sbi[M];
+struct BigInt
 {
 	int n,sgn;
 	int a[M/MB]; //10^4=10000
-	BN():n(1),sgn(1)
+	BigInt():n(0),sgn(1)
 	{
 		memset(a,0,sizeof(a));
 	}
-	BN(LL i)
+	BigInt(LL i)
 	{
 		n=0; sgn=1;
+		memset(a,0,sizeof(a));
 		if(i<0)
 		{
-			sgn=-1; i=-i;
+			sgn=-1; i=-i; //error LLMIN
 		}
 		while(i)
 		{
@@ -28,9 +36,10 @@ struct BN
 			i/=BASE;
 		}
 	}
-	BN(char s[])
+	BigInt(char s[])
 	{
 		n=0; sgn=1;
+		memset(a,0,sizeof(a));
 		int l=strlen(s);
 		int k=0,nu=0,t=1;
 		for(int i=l-1;i>=0;i--)
@@ -46,54 +55,100 @@ struct BN
 			k++;
 		}
 		a[n++]=nu;
-		rem_zero();
+		trim();
 	}
-	void rem_zero()
+	void trim()
 	{
 		while(n>1&&!a[n-1]) n--;
 	}
-	BN operator=(const BN &B)
+	BigInt operator=(const BigInt &B)
 	{
 		n=B.n; sgn=B.sgn;
+		memset(a,0,sizeof(a));
 		for(int i=0;i<n;i++)
 			a[i]=B.a[i];
+		return *this;
 	}
-	bool operator==(const BN &B) const
+	bool operator==(const BigInt &B) const
 	{
 		if(n!=B.n||sgn!=B.sgn) return 0;
 		for(int i=0;i<n;i++)
 			if(a[i]!=B.a[i]) return 0;
 		return 1;
 	}
-	bool operator<(const BN &B) const 
+	bool operator<(const BigInt &B) const 
+
 	{
 		if(sgn!=B.sgn) return sgn<B.sgn;
-		if(n!=B.n) return sgn>0 ? n<B.n : n>B.n;
+		if(n!=B.n) return n*sgn<B.n*sgn;
 		for(int i=n-1;i>=0;i--)
 			if(a[i]!=B.a[i]) 
-				return sgn>0 ? a[i]<B.a[i] : a[i]>B.a[i];
+				return a[i]*sgn<B.a[i]*sgn;
 		return 0;
 	}
-	BN operator-() const
+	bool operator>(const BigInt &B) const
 	{
-		BN ans=*this;
+		return B<*this;
+	}
+	bool operator<=(const BigInt &B) const
+	{
+		return !(*this>B);
+	}
+	bool operator>=(const BigInt &B) const
+	{
+		return !(*this<B);
+	}
+	
+	bool operator<(int v) const
+	{
+		return *this<BigInt(v);
+	}
+	BigInt operator-() const
+	{
+		BigInt ans=*this;
 		ans.sgn*=-1;
 		return ans;
 	}
-	BN abs() const
+	BigInt abs() const
 	{
-		BN ans=*this;
+		BigInt ans=*this;
 		ans.sgn=1;
 		return ans;
 	}
-	BN operator+(const BN &B) const
+	BigInt operator+(const BigInt &B) const
 	{
 		if(sgn!=B.sgn)
 			return *this-(-B);
+		BigInt ans=B;
+		int i=0,x=0;
+		for(;i<max(n,B.n)||x;i++)
+		{
+			ans.a[i]+=a[i]+x;
+			x=(ans.a[i]>=BASE);
+			if(x) ans.a[i]-=BASE;
+		}
+		ans.n=i;
+		return ans;
 	}
-	BN operator*(const BN &B) const
+	BigInt operator-(const BigInt &B) const
 	{
-		BN ans;
+		if(sgn!=B.sgn)
+			return *this+(-B);
+		if(abs()<B.abs()) 
+			return -(B-*this);
+		BigInt ans=*this;
+		for(int i=0,x=0;i<n||x;i++)
+		{
+			ans.a[i]-=x+B.a[i];
+			x=(ans.a[i]<0);
+			if(x) ans.a[i]+=BASE;
+		}
+		ans.trim();
+		return ans;
+	}
+	BigInt operator*(const BigInt &B) const
+	{
+		BigInt ans;
 		ans.n=n+B.n;
 		ans.sgn=sgn*B.sgn;
 		int x;
@@ -108,15 +163,99 @@ struct BN
 			}
 			ans.a[i+B.n]=x;
 		}
-		ans.rem_zero();
+		ans.trim();
 		return ans;
+	}
+	BigInt operator+=(const BigInt &B)
+	{
+		return *this=*this+B;
+	}
+	BigInt operator-=(const BigInt &B)
+	{
+		return *this=*this-B;
+	}
+	BigInt operator*=(const BigInt &B)
+	{
+		return *this=*this*B;
+	}
+
+	pair<BigInt,BigInt> divmod(const BigInt &B) const
+	{
+		int norm=BASE/(B.a[B.n-1]+1);
+		BigInt a=(this->abs())*norm;
+		BigInt b=(B.abs())*norm;
+		BigInt q,r;
+		q.n=a.n;
+		int s1,s2,d;
+		for(int i=a.n-1;i>=0;i--)
+		{
+			r*=BASE;
+			r+=a.a[i];
+			s1=r.n<b.n ? 0 : r.a[b.n];
+			s2=r.n<b.n-1 ? 0 : r.a[b.n-1];
+			d=((LL)BASE*s1+s2)/b.a[b.n-1];
+			r-=b*d;
+			while(r<0)
+			{
+				r+=b; d--;
+			}
+			q.a[i]=d;
+		}
+		q.sgn=sgn*B.sgn;
+		r.sgn=sgn;
+		q.trim(); r.trim();
+		return make_pair(q,r/norm);
+	}
+	BigInt operator/(const BigInt &B) const
+	{
+		return divmod(B).first;
+	}
+	BigInt operator/=(int v)
+	{
+		if(v<0)
+		{
+			sgn=-sgn; v=-v;
+		}
+		for(int i=n-1,r=0;i>=0;i--)
+		{
+			LL cur=a[i]+(LL)r*BASE;
+			a[i]=cur/v;
+			r=cur%v;
+		}
+		trim();
+		return *this;
+	}
+	BigInt operator/(int v) const
+	{
+		BigInt ans=*this;
+		ans/=v;
+		return ans;
+	}
+	BigInt operator%(const BigInt &B) const
+	{
+		return divmod(B).second;
+	}
+	int operator%(int v) const
+	{
+		if(v<0)
+			v=-v;
+		int m=0;
+		for(int i=n-1;i>=0;i--)
+			m=(a[i]+m*(LL)BASE)%v;
+		return m*sgn;
+	}
+
+	void read()
+	{
+		scanf("%s",sbi);
+		*this=BigInt(sbi);
 	}
 	void print()
 	{
 		printf("%d",a[n-1]*sgn);
 		for(int i=n-2;i>=0;i--)
 			printf("%04d",a[i]); // MB=4
-		printf("\n");
+		//printf("\n");
 	}
 };
 
@@ -129,13 +268,31 @@ char s6[]="123456789012";
 
 int main()
 {
-	BN a1(s1),a2(s2),a3(s3),a4(s4),a5(s5),a6(s6);
+	/*
+	BigInt a1(s1),a2(s2),a3(s3),a4(s4),a5(s5),a6(s6);
 	a1.print();
 	a2.print();
 	a3.print(); 
 	a4.print(); 
 	a5.print(); 
 	a6.print(); 
+	*/
+
+	srand(time(0));
+	for(int i=0;i<10;i++)
+	{
+		LL a=(rand()%2?1:-1)*((LL)rand()*rand());
+		LL b=(rand()%2?1:-1)*((LL)rand());
+		cout<<a<<" "<<b<<endl;
+		cout<<a+b<<" "<<a-b<<" "<<a*b<<" "<<a/b<<" "<<a%b<<endl;
+		BigInt A(a),B(b);
+		A.print(); cout<<" "; B.print(); cout<<endl;
+		(A+B).print(); cout<<" ";
+		(A-B).print(); cout<<" ";
+		(A*B).print(); cout<<" ";
+		(A/B).print(); cout<<" ";
+		(A%B).print(); cout<<endl<<endl;
+	}
 
 	return 0;
 }
