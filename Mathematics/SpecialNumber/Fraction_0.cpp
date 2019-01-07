4136be0d58ee8afdd4dad3b6f0ba823a10e9034d
OTTFFYZY <992241461@qq.com>
2019-01-06 21:51:46 -0500

    2019_01_06_upd_0

--- Mathematics/SpecialNumber/Fraction_0.cpp
+++ Mathematics/SpecialNumber/Fraction_0.cpp
@@ -6,18 +6,55 @@
 using namespace std;
 
 const int M=1e4+5;
+typedef long long LL;
 
 struct Frac
 {
-	int a,b; // a/b
-	int gcd(int x,int y)
+	LL a,b; // a/b
+	Frac(LL a=0,LL b=1):a(a),b(b)
+	{
+		trim();
+	}
+	int gcd(LL x,LL y)
 	{
 		return !y?x:gcd(y,x%y);
 	}
 	void trim()
 	{
-		int g=gcd(a,b);
+		int g=gcd(abs(a),abs(b));
 		a/=g; b/=g;
+		if(b<0)
+		{
+			a=-a; b=-b;
+		}
+	}
+	bool operator==(const Frac &B) const
+	{
+		return a==B.a && b==B.b;
+	}
+	bool operator<(const Frac &B) const
+	{
+		return a*B.b < B.a*b;
+	}
+	Frac operator*(const Frac &B) const
+	{
+		return Frac(a*B.a, b*B.b);
+	}
+	Frac operator+(const Frac &B) const
+	{
+		return Frac(a*B.b+B.a*b, b*B.b);
+	}
+	Frac operator-(const Frac &B) const
+	{
+		return Frac(a*B.b-B.a*b, b*B.b);
+	}
+	Frac operator/(const Frac &B) const
+	{
+		return Frac(a*B.b, b*B.a);
+	}
+	void print()
+	{
+		printf("%d/%d\n",a,b);
 	}
 };
 
