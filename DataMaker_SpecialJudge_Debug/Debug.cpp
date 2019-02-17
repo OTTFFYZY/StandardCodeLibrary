004e5ac7369eb448f7b67fd05152595b1a5a5546
OTTFFYZY <992241461@qq.com>
2019-02-16 19:03:39 -0500

    2019_02_16_upd_0

--- DataMaker_SpecialJudge_Debug/Debug.cpp
+++ DataMaker_SpecialJudge_Debug/Debug.cpp
@@ -1,28 +1,35 @@
 #include <iostream>
 #include <string>
+#include <vector>
+#include <map>
+#include <algorithm>
 using namespace std;
 
-string to_debug_string(string s)
+// to_string() 
+// int, unsigned, long long, unsigned long long
+// float, double, long double
+
+string to_string(string s)
 {
     return '"' + s + '"';
 }
-string to_debug_string(const char* s) 
+string to_string(const char* s) 
 {
-    return to_debug_string((string)s);
+    return to_string((string)s);
 }
-string to_debug_string(bool b) 
+string to_string(bool b) 
 {
     return (b ? "true" : "false");
 }
 
 template <typename A, typename B>
-string to_debug_string(pair<A, B> p) 
+string to_string(pair<A, B> p) 
 {
-    return "(" + to_debug_string(p.first) + ", " + to_debug_string(p.second) + ")";
+    return "(" + to_string(p.first) + ", " + to_string(p.second) + ")";
 }
 
 template <typename A>
-string to_debug_string(A v) 
+string to_string(A v) 
 {
     bool first = true;
     string res = "{";
@@ -30,7 +37,7 @@ string to_debug_string(A v)
     {
         if (!first) res += ", ";
         first = false;
-        res += to_debug_string(x);
+        res += to_string(x);
     }
     res += "}";
     return res;
@@ -40,21 +47,41 @@ void debug_out()
 { 
     cerr << endl; 
 }
-
 template <typename Head, typename... Tail>
 void debug_out(Head H, Tail... T) 
 {
-    cerr << " " << to_debug_string(H);
+    cerr << " " << to_string(H);
     debug_out(T...);
 }
 
 #ifdef OTTFF
     #define debug(...) cerr << "[" << #__VA_ARGS__ << "]:", debug_out(__VA_ARGS__)
 #else
-    #define debug(...) 42
+    #define debug(...) 38
 #endif
 
 int main()
 {
+    int a=10;
+    long long b=123456789012345678L;
+    double c=3.1415926;
+    debug(a,b,c);
+
+    vector<vector<int>> g(5);
+    g[0].push_back(1);
+    g[1].push_back(2); g[1].push_back(4);
+    g[2].push_back(1); g[2].push_back(3); g[2].push_back(0);
+    g[3].push_back(1); g[3].push_back(2);
+    g[4].push_back(3); g[4].push_back(1);
+    debug(g);
+
+    map<int,pair<int,int>> ma;
+    ma[0]=make_pair(2,3);
+    ma[1]=make_pair(5,3); 
+    ma[2]=make_pair(6,4); 
+    ma[3]=make_pair(7,2); 
+    ma[4]=make_pair(1,8);
+    debug(ma);
+    
     return 0;
 }
--- IntermediateDataStructure/SegmentTree_scrq_vector.cpp
+++ IntermediateDataStructure/SegmentTree_scrq_vector.cpp
@@ -9,70 +9,99 @@ const int M=1e5+5;
 const int INF=0x3f3f3f3f;
 
 int n;
-int ML,MR;
+
 struct SegT
 {
-	struct Node
+	struct TN
 	{
-
+		T val;
+		T lazy;
+		void apply(int l,int r,int v)
+		{
+			val+=v;
+			lazy+=v;
+		}
 	};
-	vector<int> tn;
-	void init(int v[],int n)
+	TN unite(const TN &a,const TN &b) const
 	{
-		ML=0; MR=n-1;
-		build(v,1,ML,MR);
+		TN res;
+		res.val = min(a.val,b.val);
+		return res;
 	}
-	void build(int v[],int o,int l,int r)
+	void push(int o,int l,int r)
+	{
+		int m=(l+r)>>1;
+		int ls=o+1,rs=o+((m-l+1)<<1);
+		if(tn[o].lazy!=0)
+		{
+			tn[ls].apply(l,m,tn[o].lazy);
+			tn[rs].apply(m+1,r,tn[o].lazy);
+			tn[o].lazy=0;
+		}
+	}
+	void pull(int o,int ls,int rs)
+	{
+		tn[o]=unite(tn[ls],tn[rs]);
+	}
+
+	int n;
+	vector<TN> tn;
+
+	void build(int o,int l,int r)
+	{
+		if(l==r) return;
+		int m=(l+r)>>1;
+		int ls=o+1,rs=o+((m-l+1)<<1);
+		build(ls,l,m);
+		build(rs,m+1,r);
+		pull(o,ls,rs);
+	}
+	void build(int o,int l,int r,const vector<int> &v)
 	{
 		if(l==r)
 		{
-			tn[o]=v[l];
+			tn[o].add=0;
+			tn[o].val=v[l];
 			return;
 		}
-		int mid=(l+r)>>1;
-		build(v,o<<1,l,mid);
-		build(v,o<<1|1,mid+1,r);
-		pushup(o);
+		int m=(l+r)>>1;
+		int ls=o+1,rs=o+((m-l+1)<<1);
+		build(ls,l,m);
+		build(rs,m+1,r);
+		pull(o,ls,rs);
 	}
-	int pushup(int o)
+	TN query(int o,int l,int r,int ql,int qr)
 	{
-		tn[o]=max(tn[o<<1],tn[o<<1|1]);
-	}
-	int ans;
-	int ql,qr;
-	int query(int ql,int qr)
-	{
-		ans=-INF;
-		this->ql=ql; this->qr=qr;
-		query();
-		return ans;
-	}
-	void query(int o=1,int l=ML,int r=MR)
-	{
-		if(ql<=l&&r<=qr)
-		{
-			ans=max(ans,tn[o]);
-			return;
-		}
-		int mid=(l+r)>>1;
-		if(ql<=mid)
-			query(o<<1,l,mid);
-		if(qr>mid)
-			query(o<<1|1,mid+1,r);
-	}
-	void add(int x,int p,int o=1,int l=ML,int r=MR)
-	{
-		if(l==r)
-		{
-			tn[o]+=x;
-			return;
-		}
-		int mid=(l+r)>>1;
-		if(p<mid)
-			add(x,p,o<<1,l,mid);
+		if(ql<=l && r<=ql)
+			return tn[o];
+		int m=(l+r)>>1;
+		int ls=o+1,rs=o+((m-l+1)<<1);
+		push(o,l,r);
+		TN res;
+		if(qr<=m)
+			res=query(ls,l,m,ql,qr);
 		else
-			add(x,p,o<<1|1,mid+1,r);
-		pushup(o);
+		{
+			if(ql>m) res=query(rs,m+1,r,ql,qr);
+			else
+				res=unite(query(ls,l,m,ql,qr),query(rs,m+1,r,ql,qr));
+		}
+		pull(o,ls,rs);
+		return res;
+	}
+	void modify(int o,int l,int r,int ml,int mr,int v)
+	{
+		if(ml<=l && r<= mr)
+		{
+			tn[o].apply(l,r,v);
+			return;
+		}
+		int m=(l+r)>>1;
+		int ls=o+1,rs=o+((m-l+1)<<1);
+		push(o,l,r);
+		if(ml<=m) modify(ls,l,m,ml,mr,v);
+		if(m<mr) modify(rs,m+1,r,ml,mr,v);
+		pull(o,ls,rs);
 	}
 };
 
