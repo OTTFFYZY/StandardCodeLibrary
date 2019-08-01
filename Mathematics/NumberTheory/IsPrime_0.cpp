e1c91a9c34ce0badf5c5e81066ee558027cf0baa
OTTFFYZY <992241461@qq.com>
2019-08-01 22:17:11 +0800

    2019_08_01_upd_0

--- Mathematics/NumberTheory/IsPrime_0.cpp
+++ Mathematics/NumberTheory/IsPrime_0.cpp
@@ -9,7 +9,7 @@ bool isprime(int n)
 	return true;
 }
 
-bool inprime2(int n)
+bool isprime2(int n)
 {
 	if(n==1) return false;
 	if(n==2||n==3) return true;
@@ -17,4 +17,19 @@ bool inprime2(int n)
 	for(int i=5;i<=n/i;i+=6)
 		if(n%i==0||n%(i+2)==0) return false;
 	return true;
+}
+
+typedef long long LL;
+bool isprime3(LL n)
+{
+	if(n==2||n==3||n==5) return 1;
+	if(n==1||n%2==0||n%3==0||n%5==0) return 0;
+	LL c=7, a[8]={4,2,4,2,4,6,2,6};
+	while(c*c<=n)
+		for(auto i:a)
+		{
+			if(n%c==0) return 0;
+			c+=i;
+		}
+	return 1;
 }
