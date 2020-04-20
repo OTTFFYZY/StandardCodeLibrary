de14cf70c6e977616b78e5f5afcea9153e698754
OTTFFYZY <992241461@qq.com>
2020-04-19 20:16:43 -0400

    2020_04_19_upd_0

--- Mathematics/NumberTheory/ModularCombinatorialNumber_0.cpp
+++ Mathematics/NumberTheory/ModularCombinatorialNumber_0.cpp
@@ -4,22 +4,54 @@ typedef long long LL;
 // n,m <=1e6
 const int M=1e6+5;
 const int MOD=1e9+7;
-LL inv[M],fa[M],ifa[M];
+inline int add(int x,int y)
+{
+	x+=y;
+	if(x>=MO) x-=MO;
+	return x;
+}
+inline int sub(int x,int y)
+{
+	x-=y;
+	if(x<0) x+=MO;
+	return x;
+}
+inline int mul(int x,int y)
+{
+	return 1LL*x*y%MO;
+}
+inline void addv(int &x,int y)
+{
+	x+=y;
+	if(x>=MO) x-=MO;
+}
+inline void subv(int &x,int y)
+{
+	x-=y;
+	if(x<0) x+=MO;
+}
+inline void mulv(int &x,int y)
+{
+	x=1LL*x*y%MO;
+}
+ 
+int inv[M],fa[M],ifa[M];
 void init()
 {
 	fa[0]=ifa[0]=1;
 	inv[1]=fa[1]=ifa[1]=1;
     for(int i=2;i<M;i++)
     {
-    	fa[i]=fa[i-1]*i%MOD;
-    	inv[i]=(MOD-MOD/i)*inv[MOD%i]%MOD;
-    	ifa[i]=inv[i]*ifa[i-1]%MOD;
+    	fa[i]=mul(fa[i-1],i);
+    	inv[i]=mul(MO-MO/i,inv[MO%i]);
+    	ifa[i]=mul(inv[i],ifa[i-1]);
     }
 }
-LL C1(LL n,LL m)
+ 
+int C(int n,int m)
 {
-	if(m>n) return 0;
-	return fa[n]*ifa[m]%MOD*ifa[n-m]%MOD;
+	if(n<m) return 0;
+	return mul(mul(fa[n],ifa[m]),ifa[n-m]);
 }
 
 
