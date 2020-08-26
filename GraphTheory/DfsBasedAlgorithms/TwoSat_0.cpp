8c971ae32fe724b423a80bbaa6f096953c044d72
OTTFFYZY <992241461@qq.com>
2020-08-26 13:07:18 -0400

    Update TwoSat_0.cpp

--- GraphTheory/DfsBasedAlgorithms/TwoSat_0.cpp
+++ GraphTheory/DfsBasedAlgorithms/TwoSat_0.cpp
@@ -8,11 +8,20 @@ using namespace std;
 const int MV=1e5+5;
 vector<int> g[MV*2];
 int nv;
+int st[MV*2],stp;
+int mk[MV*2];
 
 void init()
 {
 	for(int i=0;i<nv*2;i++)
-		g[i].clear();
+		g[i].clear(),mk[i]=0;
+}
+
+// x==vx
+void set_value(int x,int vx)
+{
+	x=x*2+vx;
+	g[x^1].push_back(x);
 }
 
 // x==vx or y==vy
@@ -23,31 +32,29 @@ void add_clause(int x,int vx,int y,int vy)
 	g[x^1].push_back(y);
 	g[y^1].push_back(x);
 }
-
-int st[MV*2],stp;
-
-int mark[MV*2];
+ 
+ 
 bool dfs(int u)
 {
-	if(mark[u^1]) return 0;
-	if(mark[u]) return 1;
-	mark[u]=1;
+	if(mk[u^1]) return 0;
+	if(mk[u]) return 1;
+	mk[u]=1;
 	st[++stp]=u;
 	for(int v:g[u])
 		if(!dfs(v)) return 0;
 	return 1;
 }
-
+ 
 bool twoSat()
 {
-	memset(mark,0,sizeof(mark));
+	//memset(mk,0,sizeof(mk));
 	for(int i=0;i<nv*2;i+=2)
 	{
 		stp=0;
 		if(!dfs(i))
 		{
 			while(stp)
-				mark[st[stp--]]=0;
+				mk[st[stp--]]=0;
 			if(!dfs(i+1)) return 0;
 		}
 	}
